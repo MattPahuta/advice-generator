ed893101ebb6bf5466b45e22f45c1b8085bce2c1
Matt Pahuta <mhpahuta@gmail.com>
2022-09-21 15:53:12 -0700

    Add initial API call script and DOM update function

--- js/app.js
+++ js/app.js
@@ -1,3 +1,28 @@
 // Advice Slip API
 // https://api.adviceslip.com/advice
 
+// get advice function
+async function getAdvice() {
+  const res = await fetch("https://api.adviceslip.com/advice");
+  const data = await res.json();
+
+  const id = data.slip.id;
+  const advice = data.slip.advice;
+  console.log(data.slip.advice)
+  console.log(data.slip.id);
+
+  displayAdvice(id, advice);
+
+}
+
+
+function displayAdvice(id, advice) {
+  const adviceId = document.querySelector('#advice-id');
+  const adviceText = document.querySelector('#advice-text');
+
+  adviceId.textContent = id;
+  adviceText.textContent = advice;
+
+}
+
+getAdvice()
