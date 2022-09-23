8ba854752d8897f77e35bc3abdcfb2e331471d46
Matt Pahuta <mhpahuta@gmail.com>
2022-09-23 15:06:50 -0700

    Add advice button functionality, desktop style adjustments

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@
 async function getAdvice() {
   const res = await fetch("https://api.adviceslip.com/advice");
   const data = await res.json();
-
+  console.log('calling getAdvice...')
   const id = data.slip.id;
   const advice = data.slip.advice;
   console.log(data.slip.advice)
@@ -14,15 +14,14 @@ async function getAdvice() {
   displayAdvice(id, advice);
 
 }
-
+// initial call for advice to populate at page load
+// getAdvice(); 
 
 function displayAdvice(id, advice) {
   const adviceId = document.querySelector('#advice-id');
   const adviceText = document.querySelector('#advice-text');
-
   adviceId.textContent = id;
   adviceText.textContent = advice;
-
 }
 
-getAdvice()
+document.querySelector('#advice-btn').addEventListener('click', getAdvice);
